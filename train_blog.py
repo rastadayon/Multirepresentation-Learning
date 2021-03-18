8c3c723e88184344a6c72456db88da7d49ba21e4
Rasta <rasta.tadayon1378@gmail.com>
2021-03-19 01:13:20 +0330

    Add configuration for two resnet50s

--- train_blog.py
+++ train_blog.py
@@ -7,33 +7,44 @@ from moco import MoCoMethodParams
 
 
 def main():
-    base_config = MoCoMethodParams(
-        lr=0.8,
-        batch_size=256,
-        gather_keys_for_queue=False,
-        loss_type="ip",
-        use_negative_examples_from_queue=False,
-        use_both_augmentations_as_queries=True,
-        mlp_normalization="bn",
-        prediction_mlp_layers=2,
-        projection_mlp_layers=2,
-        m=0.996,
-        use_momentum_schedule=True,
+    # base_config = MoCoMethodParams(
+    #     lr=0.8,
+    #     batch_size=256,
+    #     gather_keys_for_queue=False,
+    #     loss_type="ip",
+    #     use_negative_examples_from_queue=False,
+    #     use_both_augmentations_as_queries=True,
+    #     mlp_normalization="bn",
+    #     prediction_mlp_layers=2,
+    #     projection_mlp_layers=2,
+    #     m=0.996,
+    #     use_momentum_schedule=True,
+    # )
+    two_resnet50s = MoCoMethodParams(
+    use_eqco_margin=True, 
+    eqco_alpha=65536, 
+    K=0,
+    use_negative_examples_from_batch=True,
+    use_negative_examples_from_queue=False,
+    max_epochs=200,
+    encoder_arch="resnet50",
+    embedding_dim=2048
     )
+    base_config = two_resnet50s
     configs = {
         "base": base_config,
-        "pred_only": evolve(base_config, mlp_normalization=None, prediction_mlp_normalization="bn"),
-        "proj_only": evolve(base_config, mlp_normalization="bn", prediction_mlp_normalization=None),
-        "no_norm": evolve(base_config, mlp_normalization=None),
-        "layer_norm": evolve(base_config, mlp_normalization="ln"),
-        "xent": evolve(base_config, use_negative_examples_from_queue=True, loss_type="ce", mlp_normalization=None, lr=0.02),
+        # "pred_only": evolve(base_config, mlp_normalization=None, prediction_mlp_normalization="bn"),
+        # "proj_only": evolve(base_config, mlp_normalization="bn", prediction_mlp_normalization=None),
+        # "no_norm": evolve(base_config, mlp_normalization=None),
+        # "layer_norm": evolve(base_config, mlp_normalization="ln"),
+        # "xent": evolve(base_config, use_negative_examples_from_queue=True, loss_type="ce", mlp_normalization=None, lr=0.02),
     }
-    for seed in range(3):
+    for seed in range(1):
         for name, config in configs.items():
             method = MoCoMethod(config)
             logger = TensorBoardLogger("tb_logs", name=f"{name}_{seed}")
 
-            trainer = pl.Trainer(gpus=1, max_epochs=10, logger=logger)
+            trainer = pl.Trainer(gpus=1, max_epochs=base_config.max_epochs, logger=logger)
 
             trainer.fit(method)
 
